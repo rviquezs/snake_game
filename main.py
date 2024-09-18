2f9d087d9570cefbaf65b2330385b970ead0c5bc
rviquez <146051750+rvqzs@users.noreply.github.com>
2024-09-18 13:29:18 -0600

    Add auto replay

--- main.py
+++ main.py
@@ -33,14 +33,14 @@ while game_is_on:
         score.increase_score()
 
     # Detect collision walls
-    if snake.head.xcor() > 290 or snake.head.xcor() < -290 or snake.head.ycor() > 290 or snake.head.ycor() < -290:
-        game_is_on = False
-        score.game_over()
+    if snake.head.xcor() > 295 or snake.head.xcor() < -295 or snake.head.ycor() > 295 or snake.head.ycor() < -295:
+        score.reset()
+        snake.reset()
 
     # Detect collision with tail
     for segment in snake.segments[1:]:
         if snake.head.distance(segment) < 10:
-            game_is_on = False
-            score.game_over()
+            score.reset()
+            snake.reset()
 
 screen.exitonclick()
--- snake.py
+++ snake.py
@@ -33,6 +33,13 @@ class Snake:
     def extend(self):
         self.add_segment(self.segments[-1].position())
 
+    def reset(self):
+        for seg in self.segments:
+            seg.goto(2000,2000)
+        self.segments.clear()
+        self.create_snake()
+        self.head = self.segments[0]
+
     def move(self):
         for seg in range(len(self.segments) - 1, 0, -1):
             x_axis = self.segments[seg - 1].xcor()
