437cb87605998854a8291bb45cc0155a51cb7866
rviquez <146051750+rvqzs@users.noreply.github.com>
2024-09-18 10:46:11 -0600

    Add food class

--- food.py
+++ food.py
@@ -1,2 +1,20 @@
-class Food:
-    pass
+from random import randint
+from turtle import Turtle
+
+class Food(Turtle):
+    def __init__(self):
+        super().__init__()
+        self.shape("circle")
+        self.penup()
+        self.shapesize(stretch_len=0.5,stretch_wid=0.5)
+        self.color("blue")
+        self.speed("fastest")
+        x_axis = randint(-280, 280)
+        y_axis = randint(-280, 280)
+        self.goto(x_axis, y_axis)
+
+    def new_location(self):
+        x_axis = randint(-280, 280)
+        y_axis = randint(-280, 280)
+        self.goto(x_axis, y_axis)
+
