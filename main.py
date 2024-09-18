558feb98ed86e61b6d118e4ef5f0d8624350b749
rviquez <146051750+rvqzs@users.noreply.github.com>
2024-09-18 10:49:07 -0600

    Add detection of collision with tail

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ from turtle import Screen
 from snake import Snake
 
 snake = Snake()
+food = Food()
+score = Score()
 
 screen = Screen()
 screen.setup(height=600, width=500)
@@ -27,8 +29,15 @@ while game_is_on:
         food.new_location()
         snake.extend()
         score.increase_score()
+
     # Detect collision walls
     if snake.head.xcor() > 290 or snake.head.xcor() < -290 or snake.head.ycor() > 290 or snake.head.ycor() < -290:
         game_is_on = False
         score.game_over()
+    # Detect collision with tail
+    for segment in snake.segments[1:]:
+        if snake.head.distance(segment) < 10:
+            game_is_on = False
+            score.game_over()
+
 screen.exitonclick()
