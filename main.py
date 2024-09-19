a083328c74b9c98c0cf2fa2a41cac4e7d9578be8
rviquez <146051750+rvqzs@users.noreply.github.com>
2024-09-19 00:02:14 -0600

    Update main.py

--- main.py
+++ main.py
@@ -4,43 +4,48 @@ from snake import Snake
 from food import Food
 from score import Score
 
-snake = Snake()
-food = Food()
-score = Score()
+def main():
+    snake = Snake()
+    food = Food()
+    score = Score()
 
-screen = Screen()
-screen.setup(height=600, width=600)
-screen.bgcolor("black")
-screen.title("Snake Game")
-screen.tracer(0)
-screen.listen()
-screen.onkey(snake.up,"Up")
-screen.onkey(snake.down,"Down")
-screen.onkey(snake.left,"Left")
-screen.onkey(snake.right,"Right")
+    screen = Screen()
+    screen.setup(height=600, width=600)
+    screen.bgcolor("black")
+    screen.screensize(600,600)
+    screen.title("Snake Game")
+    screen.tracer(0)
+    screen.listen()
+    screen.onkey(snake.up,"Up")
+    screen.onkey(snake.down,"Down")
+    screen.onkey(snake.left,"Left")
+    screen.onkey(snake.right,"Right")
 
-game_is_on = True
+    game_is_on = True
 
-while game_is_on:
-    screen.update()
-    time.sleep(.1)
-    snake.move()
+    while game_is_on:
+        screen.update()
+        time.sleep(.1)
+        snake.move()
 
-    # Detect collision food
-    if snake.head.distance(food) < 15:
-        food.new_location()
-        snake.extend()
-        score.increase_score()
+        # Detect collision food
+        if snake.head.distance(food) < 15:
+            food.new_location()
+            snake.extend()
+            score.increase_score()
 
-    # Detect collision walls
-    if snake.head.xcor() > 295 or snake.head.xcor() < -295 or snake.head.ycor() > 295 or snake.head.ycor() < -295:
-        score.reset()
-        snake.reset()
-
-    # Detect collision with tail
-    for segment in snake.segments[1:]:
-        if snake.head.distance(segment) < 10:
+        # Detect collision walls
+        if snake.head.xcor() > 295 or snake.head.xcor() < -295 or snake.head.ycor() > 295 or snake.head.ycor() < -295:
             score.reset()
             snake.reset()
 
-screen.exitonclick()
+        # Detect collision with tail
+        for segment in snake.segments[1:]:
+            if snake.head.distance(segment) < 10:
+                score.reset()
+                snake.reset()
+
+    screen.exitonclick()
+
+if __name__ == "__main__":
+    main()
